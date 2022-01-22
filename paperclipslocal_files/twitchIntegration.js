1accc4b75c8247cb93586de8a8584b0bbf75c111
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-01-22 18:29:39 -0500

    comfy integrated, connection established

--- paperclipslocal_files/twitchIntegration.js
+++ paperclipslocal_files/twitchIntegration.js
@@ -1,1 +1,5 @@
 console.log('connected')
+ComfyJS.Init("FrostyyPaws", oAuthToken)
+ComfyJS.onReward = (user, reward, cost, message, extra) => {
+    console.log(user, reward, cost, message, extra)
+}
